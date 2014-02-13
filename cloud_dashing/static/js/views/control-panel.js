4f01fb914841c77862f2e49548f52595780be285
xiechao06 <xiechao06@gmail.com>
2014-02-13 18:50:22 +0800

    fix #4

--- cloud_dashing/static/js/views/control-panel.js
+++ cloud_dashing/static/js/views/control-panel.js
@@ -15,24 +15,36 @@ define(['jquery', 'backbone', 'handlebars', 'text',
 
             events: {
                 'change select': '_onViewpointSet',
-                'click li.list-group-item': '_toggleAgent',
+                'click li.list-group-item': function (e) { 
+                    this._toggleAgent($(e.target)); 
+                },
+                'click li.list-group-item i': function (e) { 
+                    this._toggleAgent($(e.target).parent()); 
+                    return false;
+                },
+                'click li.list-group-item small': function (e) { 
+                    this._toggleAgent($(e.target).parent()); 
+                    return false;
+                }
             },
 
             _onViewpointSet: function (e) {
                 this.trigger('viewpoint-set', agents.get(this.$('select').val()).toJSON());
             },
 
-            _toggleAgent: function (e) {
-                var el = $(e.target);
+            _toggleAgent: function (el) {
                 var enabled = !el.attr('data-enabled');
                 el.attr('data-enabled', enabled? 'true': '');
                 el.toggleClass('list-group-item-success', enabled);
                 el.toggleClass('text-muted', !enabled);
                 el.children('i').toggleClass('fa-check', enabled);
+                var square = el.children('i.fa-square');
                 if (enabled) {
                     el.css('text-decoration', '');
+                    square.css('color', square.attr('data-color'));
                 } else {
                     el.css('text-decoration', 'line-through');
+                    square.css('color', '#ccc');
                 }
                 var agent = agents.get(el.val());
                 agent.set('selected', enabled);
@@ -48,7 +60,7 @@ define(['jquery', 'backbone', 'handlebars', 'text',
                     } else {
                         latency += 'ms';
                     }
-                    var el = this.$('li.list-group-item').eq(i);
+                    var el = this.$('li.list-group-item').eq(i).find('small');
                     el.text(el.text().replace(/-.*/, '- ' + latency));
                 });
             }
