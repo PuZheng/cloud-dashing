1d4b5946a207d0a7ee8ccdaf814c05c8941e502c
xiechao06 <xiechao06@gmail.com>
2014-03-07 15:23:25 +0800

    fix table-view

--- cloud_dashing/static/js/views/table-view.js
+++ cloud_dashing/static/js/views/table-view.js
@@ -1,15 +1,12 @@
-define(['jquery', 'backbone'],
-    function ($, Backbone) {
+define(['jquery', 'backbone', 'handlebars', 'text!/static/templates/table-view.hbs'],
+    function ($, Backbone, Handlebars, tableViewTemplate) {
         var TableView = Backbone.View.extend({
-            _table: function (data) {
-                return $("<table></table>").addClass("table table-bordered table-condensed").append(this._renderDetail(data));
-            },
-
+            _template: Handlebars.default.compile(tableViewTemplate),
             _renderService: function (name, vals) {
-                var th = $("<tr></tr>").append($("<th></th>").attr("rowspan", 2).text(name).addClass("text-center"));
+                var th = $("<tr></tr>").append($("<th></th>").attr("rowspan", 2).html(name + '<i class="fa fa-question-circle fa-fw"></i>').addClass("text-center").attr('valign', 'middle').css('vertical-align', 'middle'));
                 var tds = [];
                 _.each(vals, function (val, key) {
-                    th.append($("<th></th>").addClass("text-center").text(key));
+                    th.append($("<th></th>").addClass("text-center").html(key));
                     if (typeof val == 'boolean') {
                         if (val) {
                             val = '是';
@@ -35,13 +32,15 @@ define(['jquery', 'backbone'],
             updateTimeSpot: function (data) {
                 this.$el.empty();
                 if (!!data[0]) {
-                    this.$el.append(this._table(data[0])).append(this._desc());
+                    this.$el.append(this._template({agent: this._viewpoint}));
+                    this.$('table').append(this._renderDetail(data[0]));
                 }
             },
 
             _desc: function () {
                 return $("<p></p>").text(this._viewpoint.provider + "服务一览表");
             },
+
             updateViewpoint:  function(viewpoint) {
                 this._viewpoint = viewpoint;
             }
