65a5ae2a1c118a835a7ce0f0c1190a2e3d5a9887
xiechao06 <xiechao06@gmail.com>
2014-04-03 12:08:54 +0800

    fix

---
+++ cloud_dashing/static/js/jquery-plugins.js
@@ -0,0 +1,6 @@
+define(['jquery'], function ($) {
+	// Only once jquery has loaded, load the jQuery plugins
+	require(['jquery.plot.crosshair', 'jquery.plot.time', 'jquery.plot.tooltip', 'jquery.plot.symbol'], function () {
+		// Do nothing
+	});
+});
