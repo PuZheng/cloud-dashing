ec829ceba3d4265b13f9952b696d09b152540a6b
xiechao06 <xiechao06@gmail.com>
2014-07-05 12:41:18 +0800

    trivial

--- cloud_dashing/build.js
+++ cloud_dashing/build.js
@@ -89,7 +89,6 @@
             exclude: [
                 'jquery.plot.crosshair', 
                 'jquery.plot.time', 
-                'jquery.plot.tooltip', 
                 'jquery.plot.symbol'
                     ],
         },
